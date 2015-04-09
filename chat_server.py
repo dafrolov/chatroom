09295e74f8a4c97bc6e6db249db6fbec837f003c
Dmitry Frolov <dmalfrolov@mail.ru>
2015-04-09 23:30:11 +0400

    Processing of user commands added, rooms basic functionality added.

--- chat_server.py
+++ chat_server.py
@@ -7,50 +7,110 @@ from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 
 
-class Chat(LineReceiver):
+class ChatProtocol(LineReceiver):
 
-    def __init__(self, users):
-        self.users = users
+    def __init__(self, master):
+        self.chats_by_users = master.chats_by_users
+        self.users_by_rooms = master.users_by_rooms
+
         self.name = None
-        self.state = "GETNAME"
+        self.state = 'GETNAME'
 
     def connectionMade(self):
         self.sendLine("What's your name?")
 
     def connectionLost(self, reason):
-        if self.users.has_key(self.name):
-            del self.users[self.name]
+        if self.name in self.chats_by_users:
+            del self.chats_by_users[self.name]
 
     def lineReceived(self, line):
-        if self.state == "GETNAME":
-            self.handle_GETNAME(line)
+        if line.startswith('\\'):
+            line = line.lstrip('\\')
+            self.handle_command(line)
         else:
-            self.handle_CHAT(line)
+            self.handle_state(line)
 
-    def handle_GETNAME(self, name):
-        if self.users.has_key(name):
-            self.sendLine("Name taken, please choose another.")
+    def handle_state(self, line):  # TODO: StateHandler
+        try:
+            method = getattr(self, 'handle_state_' + self.state)
+        except AttributeError:
+            pass
+        else:
+            result = method(line)
+
+    def handle_command(self, line):  # TODO: CommandHandler
+        try:
+            command, params = line.split(' ', 1)
+        except ValueError:
+            self.sendLine('Invalid command format: "%s"' % line)
             return
-        self.sendLine("Welcome, %s!" % (name,))
-        self.name = name
-        self.users[name] = self
-        self.state = "CHAT"
 
-    def handle_CHAT(self, message):
-        message = "<%s> %s" % (self.name, message)
-        for name, protocol in self.users.iteritems():
+        try:
+            method = getattr(self, 'handle_command_' + command)  # TODO: method_by_command_name
+        except AttributeError:
+            self.sendLine('Unknown command: %s' % command)
+            return
+        else:
+            result = method(params)
+
+    def handle_state_GETNAME(self, name):
+        if name in self.chats_by_users:
+            self.sendLine('Name taken, please choose another.')
+            return
+
+        self.sendLine('Welcome, %s!' % (name, ))
+        self.name = name
+        self.chats_by_users[name] = self
+        self.state = 'CHAT'
+
+    def handle_state_GETPASSWORD(self, password):
+        pass
+
+    def handle_state_CHAT(self, message):  # TODO: lowercase state names
+        message = '<%s> %s' % (self.name, message)
+        for name, protocol in self.chats_by_users.iteritems():
             if protocol != self:
                 protocol.sendLine(message)
+
+    def handle_command_join_room(self, room):
+        if room not in self.users_by_rooms:
+            self.users_by_rooms[room] = [self.name]
+            self.sendLine('You created room %s' % room)
+
+        elif self.name in self.users_by_rooms[room]:
+            self.sendLine('Already in room %s' % room)
+
+        else:
+            self.users_by_rooms[room].append(self.name)
+            self.sendLine('You joined room %s' % room)
+            # TODO: inform other users
+
+        print self.users_by_rooms
+
+    def handle_command_left_room(self, room):
+        if room not in self.users_by_rooms:
+            self.sendLine('Room %s does not exist' % room)
+
+        elif self.name in self.users_by_rooms[room]:
+            self.users_by_rooms[room].remove(self.name)
+            self.sendLine('You left room %s ' % room)
+            # TODO: inform other users
+
+        else:
+            self.sendLine('You are not in room %s ' % room)
+
+        print self.users_by_rooms
 
 
 class ChatFactory(Factory):
 
     def __init__(self):
-        self.users = {} # maps user names to Chat instances
+        self.chats_by_users = {}
+        self.users_by_rooms = {}
 
     def buildProtocol(self, addr):
-        return Chat(self.users)
+        return ChatProtocol(self)
 
 
-reactor.listenTCP(8123, ChatFactory())
+reactor.listenTCP(8123, ChatFactory())  # TODO separate starter with optparse
 reactor.run()
