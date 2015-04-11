285171eb851616b0a651ee22299112d34e56bc05
Dmitry Frolov <dmalfrolov@mail.ru>
2015-04-11 14:10:55 +0400

    Runner optparse added.

--- run_server.py
+++ run_server.py
@@ -1,3 +1,5 @@
+from optparse import OptionParser
+
 from twisted.internet import reactor
 
 from chat_factory import ChatFactory
@@ -6,8 +8,28 @@ import db
 
 
 if __name__ == "__main__":
-    # TODO optparse
+    parser = OptionParser()
+
+    parser.add_option(
+        "-a", "--address",
+        dest="address",
+        type="str",
+        default="127.0.0.1",
+        help="Chat server ip address",
+    )
+
+    parser.add_option(
+        "-p", "--port",
+        dest="port",
+        type="int",
+        default=8123,
+        help="Chat server port",
+    )
+
+    options, args = parser.parse_args()
+
     factory = ChatFactory()
-    reactor.listenTCP(8123, factory)
+    reactor.listenTCP(options.port, factory, interface=options.address)
     reactor.callLater(5, factory.startCommitLoop)
+
     reactor.run()
