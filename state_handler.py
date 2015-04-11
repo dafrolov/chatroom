44a341dbb33f9066fdd94f5812a9654b44a238bf
Dmitry Frolov <dmalfrolov@mail.ru>
2015-04-11 12:09:02 +0400

    Added user authorization by password.

--- state_handler.py
+++ state_handler.py
@@ -16,22 +16,34 @@ class StateHandler():
             self.sendLine('Chat session for user %s already opened.' % name)
             return
 
-        self.model = self.session.query(User).filter_by(name=name).first()
-        if self.model is None:
-            self.model = User(name)
-            self.model.set_password('1234556700000')  # TODO: don't store pwd itself, store salted hash
-            self.session.add(self.model)
-
-        self.sendLine('Welcome, %s!' % (name, ))
         self.name = name
         self.chats_by_users[name] = self
 
-        self.state = 'chat'
+        self.model = self.session.query(User).filter_by(name=name).first()
+        if self.model is None:
+            self.model = User(name)
+            self.session.add(self.model)
+            self.state = 'set_password'
+            self.sendLine('Set password for %s' % self.name)
+        else:
+            self.state = 'check_password'
+            self.sendLine('Enter password for %s' % self.name)
+
 
         print [user.name for user in self.session.query(User).all()]
 
-    def handle_get_password(self, password):
-        pass
+    def handle_set_password(self, password):
+        self.model.set_password(password)  # TODO: don't store pwd itself, store salted hash
+        self.state = 'chat'
+        self.sendLine('Registered username %s' % (self.name, ))
+        self.sendLine('Welcome, %s!' % (self.name, ))
+
+    def handle_check_password(self, password):
+        if self.model.password == password:
+            self.state = 'chat'
+            self.sendLine('Welcome, %s!' % (self.name, ))
+        else:
+            self.sendLine('Incorrect password, try again:')
 
     def handle_chat(self, message):
         message = '<%s> %s' % (self.name, message)
