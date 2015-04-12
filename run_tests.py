db8dea3bd7d48890bd8d9266524cab1ad7bf4035
Dmitry Frolov <dmalfrolov@mail.ru>
2015-04-12 16:05:16 +0400

    First test added.

---
+++ run_tests.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+
+
+def main():
+    suite = unittest.TestLoader().discover('./test')
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    no_errors = len(result.errors) + len(result.failures) == 0
+    sys.exit(0 if no_errors else 1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/test.py
@@ -0,0 +1,94 @@
+import unittest
+
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet.task import deferLater
+
+from chat_factory import ChatFactory
+
+from sqlalchemy.orm import sessionmaker
+from db import engine
+
+
+class ClientProtocol(Protocol):
+    def __init__(self):
+        pass
+
+    def connectionMade(self):
+        pass
+
+    def dataReceived(self, data):
+        self.output_buffer.append(data)
+
+        if self.input_buffer:
+            self.transport.write(self.input_buffer.pop(0) + '\r\n')
+        else:
+            self.terminate()
+
+
+class ClientFactory(Factory):
+    def __init__(self):
+        self.output_buffer = []
+        self.input_buffer = [
+            'Alice',
+            '123456',
+        ]
+
+    def buildProtocol(self, addr):
+        p = ClientProtocol()
+        p.output_buffer = self.output_buffer
+        p.input_buffer = self.input_buffer
+        p.terminate = self.terminate
+        return p
+
+    def startedConnecting(self, addr):
+        pass
+
+    def clientConnectionLost(self, connector, reason):
+        self.terminate()
+
+    def clientConnectionFailed(self, _, reason):
+        self.terminate()
+
+
+class TestChat(unittest.TestCase):
+    address = '127.0.0.1'
+    port = 8123
+
+    def test_01_login_with_registration(self):
+        self.make_server()
+        self.set_listener()
+
+        self.make_client()
+        self.set_client()
+
+        reactor.run()
+
+        expected = [
+            'Login:\r\n',
+            'Nickname Alice is free.\r\nSet password for user Alice to register it:\r\n',
+            'Registered username Alice.\r\nYou are logged in as Alice.\r\n',
+        ]
+
+        self.assertListEqual(self.client.output_buffer, expected)
+
+    def make_server(self):
+        session = sessionmaker(bind=engine)()
+        self.server = ChatFactory(session)
+
+    def set_listener(self):
+        self.listener = reactor.listenTCP(self.port, self.server, interface=self.address)
+
+    def terminate(self):
+        deferLater(reactor, 0, self.listener.stopListening) \
+            .addCallback(lambda ignored: reactor.crash())
+
+    def make_client(self):
+        self.client = ClientFactory()
+        self.client.terminate = self.terminate
+
+    def connect(self):
+        reactor.connectTCP(self.address, self.port, self.client)
+
+    def set_client(self):
+        reactor.callWhenRunning(reactor.callLater, 2, self.connect)
